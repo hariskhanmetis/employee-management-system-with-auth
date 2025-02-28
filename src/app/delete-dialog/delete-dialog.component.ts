9b26af4ae758c26744fc118bb4508c03e3ac3f1e
hariskhanmetis <hariskhan@metispvt.com>
2025-02-28 09:56:57 +0500

    Code Optimizations!

--- src/app/delete-dialog/delete-dialog.component.ts
+++ src/app/delete-dialog/delete-dialog.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../services/auth.service';
 import { MatDialogRef } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { EmployeeTableComponent } from '../employee-table/employee-table.component';
 
 @Component({
   selector: 'app-delete-dialog',
@@ -11,8 +8,6 @@ import { EmployeeTableComponent } from '../employee-table/employee-table.compone
 })
 export class DeleteDialogComponent {
   constructor(
-    private authService: AuthService,
-    private router: Router,
     private dialogRef: MatDialogRef<DeleteDialogComponent>
   ) { }
 
