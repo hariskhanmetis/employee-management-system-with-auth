6308a63727c9c4131c5c7993109363865a33997f
hariskhanmetis <hariskhan@metispvt.com>
2025-03-05 14:00:02 +0500

    Added am4 pie chart in overview, styling on going...

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent {
+export class OverviewComponent implements AfterViewInit, OnDestroy {
+  private chart!: am4charts.PieChart;
+
+  @ViewChild('chartDiv', { static: false }) chartDiv!: ElementRef;
+
+  constructor(private http: HttpClient) {}
+
+  ngAfterViewInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.http.get<Employee[]>('http://localhost:3000/employees').subscribe(data => {
+      this.createChart(data);
+    });
+  }
+
+  createChart(employees: Employee[]) {
+    am4core.useTheme(am4themes_animated);
+    const categoryCounts: { [key: string]: number } = {};
+  
+    for (const emp of employees) {
+      categoryCounts[emp.category] = (categoryCounts[emp.category] || 0) + 1;
+    }
+  
+    const chartData = Object.entries(categoryCounts).map(([category, count]) => ({
+      category,
+      count
+    }));
+  
+    let chart = am4core.create(this.chartDiv.nativeElement, am4charts.PieChart);
+    chart.data = chartData;
+  
+    let pieSeries = chart.series.push(new am4charts.PieSeries());
+    pieSeries.dataFields.value = "count";
+    pieSeries.dataFields.category = "category";
+    pieSeries.slices.template.stroke = am4core.color("#fff");
+    pieSeries.slices.template.strokeOpacity = 1;
+  
+    pieSeries.hiddenState.properties.opacity = 1;
+    pieSeries.hiddenState.properties.endAngle = -90;
+    pieSeries.hiddenState.properties.startAngle = -90;
+    chart.hiddenState.properties.radius = am4core.percent(0);
+  
+    this.chart = chart;
+  }
+  
+
+  ngOnDestroy() {
+    if (this.chart) {
+      this.chart.dispose();
+    }
+  }
 
 }
