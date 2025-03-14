9fa0209d8da2e169a788bf244e8f0c2f626b6ab0
hariskhanmetis <hariskhan@metispvt.com>
2025-03-14 14:23:57 +0500

    authchildGuard restored.

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -31,6 +31,7 @@ import { OverviewComponent } from '../overview/overview.component';
 import { SettingsComponent } from '../settings/settings.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatBadgeModule } from '@angular/material/badge';
+import { authchildGuard } from '../auth/authchild.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'employee-details', pathMatch: 'full' },
