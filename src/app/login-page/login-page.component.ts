6ac214d920b6cf4be3284978ed1c6a558dc9503a
hariskhanmetis <hariskhan@metispvt.com>
2025-03-26 08:43:23 +0500

    Adjusted some snackbars positions.

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -39,9 +39,9 @@ export class LoginPageComponent {
       this.authService.login(email, password).subscribe(success => {
         if (success) {
           this.router.navigate(['employee']);
-          this.snackBar.open('Login Successful!', 'Close', { duration: 3000, horizontalPosition: 'left', verticalPosition: 'bottom' });
+          this.snackBar.open('Login Successful!', 'Close', { duration: 3000, horizontalPosition: 'center', verticalPosition: 'bottom' });
         } else {
-          this.snackBar.open('Please Enter Valid Credentials!', 'Close', { duration: 3000, horizontalPosition: 'right', verticalPosition: 'top' });
+          this.snackBar.open('Please Enter Valid Credentials!', 'Close', { duration: 3000, horizontalPosition: 'center', verticalPosition: 'top' });
         }
       });
     }
